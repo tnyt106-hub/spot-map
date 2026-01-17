5172cca7a7aec1a2869cd5fb0b9463ea92723f9f
tnyt106-hub <tn.yt106@gmail.com>
2026-01-17 23:46:52 +0900

    Update map.js

--- docs/js/map.js
+++ docs/js/map.js
@@ -149,7 +149,6 @@ if (locateBtn) {
 // 検索ボックス処理
 // =======================
 const searchInput = document.getElementById("search-input");
-//const searchBtn = document.getElementById("search-btn");
 const suggestions = document.getElementById("search-suggestions");
 
 function clearSuggestions() {
@@ -203,9 +202,6 @@ function executeSearch() {
     firstHit.openPopup();
   }
 }
-
-searchBtn.addEventListener("click", executeSearch);
-
 searchInput.addEventListener("keydown", e => {
   if (e.key === "Enter") executeSearch();
 });
